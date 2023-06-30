a8091f08d7e342e30ed9ebdaedda85e2350dd1ef
Ariana Osório <ariana.osorio@parceiros.nos.pt>
2023-06-30 17:04:44 +0100

    Refactoring the code

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -37,19 +37,14 @@ def save_data(dataset: pd.DataFrame, file_name: str, data_dir_path: str) -> None
     dataset.to_csv(os.path.join(data_dir_path, file_name), index=False)
 
 
-def clean_data(
-    file_path: str, file_name: str, data_dir_path: str, country: str = "PT"
-) -> None:
+def clean_data(dataset: pd.DataFrame, country: str = "PT") -> pd.DataFrame:
     """
     Clean the raw data.
 
-    :param file_path: The file path.
-    :param file_name: The name of the file to save.
-    :param data_dir_path: The path of data directory.
+    :param dataset: A pandas dataframe with the data to clean.
     :param country: The name of country.
+    :returns: A pandas dataframe with the cleaned data.
     """
-    dataset = load_data(file_path)
-
     dataset[["unit", "sex", "age", "region"]] = dataset[
         "unit,sex,age,geo\\time"
     ].str.split(",", expand=True)
@@ -67,7 +62,23 @@ def clean_data(
 
     dataset_output = clean_dataset[clean_dataset.region == country]
 
-    save_data(dataset_output, file_name, data_dir_path)
+    return dataset_output
+
+
+def main(
+    file_path: str, file_name: str, data_dir_path: str, country: str = "PT"
+) -> None:
+    """
+    Loads and clean raw dataset. Saves the cleaned dataset.
+
+    :param file_path: The file path.
+    :param file_name: The name of the file to save.
+    :param data_dir_path: The path of data directory.
+    :param country: The name of country.
+    """
+    dataset = load_data(file_path)
+    clean_dataset = clean_data(dataset, country)
+    save_data(clean_dataset, file_name, data_dir_path)
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -76,4 +87,4 @@ if __name__ == "__main__":  # pragma: no cover
         "--country", type="str", default="PT", help="The country code to clean data for"
     )
     args = parser.parse_args()
-    clean_data(FILE_PATH, FILE_NAME, DATA_DIR, args.country)
+    main(FILE_PATH, FILE_NAME, DATA_DIR, args.country)
